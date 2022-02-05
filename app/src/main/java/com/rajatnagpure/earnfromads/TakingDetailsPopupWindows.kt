25c88c8dc58ffa1f288af5016d2d4973e9571754
rajatnagpure <rajatnagpure.rn@gmail.com>
2022-02-05 18:18:35 +0530

    adding age field

--- app/src/main/java/com/rajatnagpure/earnfromads/TakingDetailsPopupWindows.kt
+++ app/src/main/java/com/rajatnagpure/earnfromads/TakingDetailsPopupWindows.kt
@@ -21,6 +21,8 @@ class TakingDetailsPopupWindows {
     private var titleText: TextView? = null
     private var nameTextInputLayout: TextInputLayout? = null
     private var nameTextInputEditText: TextInputEditText? = null
+    private var ageTextInputLayout: TextInputLayout? = null
+    private var ageTextInputEditText: TextInputEditText? = null
     private var phoneTextInputLayout: TextInputLayout? = null
     private var phoneTextInputEditText: TextInputEditText? = null
     private var emailTextInputLayout: TextInputLayout? = null
@@ -55,10 +57,12 @@ class TakingDetailsPopupWindows {
 
         titleText = popupView.findViewById(R.id.title_text)
         nameTextInputLayout = popupView.findViewById(R.id.name_text_input)
+        ageTextInputLayout = popupView.findViewById(R.id.age_text_input)
         phoneTextInputLayout = popupView.findViewById(R.id.phone_text_input)
         emailTextInputLayout = popupView.findViewById(R.id.email_text_input)
         addressTextInputLayout = popupView.findViewById(R.id.address_text_input)
         nameTextInputEditText = popupView.findViewById(R.id.name_edit_text)
+        ageTextInputEditText = popupView.findViewById(R.id.age_edit_text)
         phoneTextInputEditText = popupView.findViewById(R.id.phone_edit_text)
         emailTextInputEditText = popupView.findViewById(R.id.email_edit_text)
         addressTextInputEditText = popupView.findViewById(R.id.address_edit_text)
@@ -66,7 +70,7 @@ class TakingDetailsPopupWindows {
 
         submitButton?.setOnClickListener{
             if(!redeemed){
-                if(validateName() && validatePhone() && validateEmail() && validateAddress()){
+                if(validateName() && validateAge() && validatePhone() && validateEmail() && validateAddress()){
                     redeemed = !redeemed
                     submitButton?.text = "Close"
                     titleText?.text = "REDEEMED!"
@@ -98,6 +102,11 @@ class TakingDetailsPopupWindows {
             if(!Patterns.PHONE.matcher(phoneTextInputEditText?.text.toString()).matches()){
                 phoneTextInputLayout?.isErrorEnabled = true
                 phoneTextInputLayout?.error = "Invalid Phone No."
+                if(phoneTextInputEditText?.text.toString().length < 10){
+                    phoneTextInputLayout?.isErrorEnabled = true
+                    phoneTextInputLayout?.error = "At Least 10 Digits Needed"
+                    return false
+                }
             }else{
                 return true
             }
@@ -113,11 +122,25 @@ class TakingDetailsPopupWindows {
                 return true
             }else{
                 nameTextInputLayout?.isErrorEnabled = true
-                nameTextInputLayout?.error = "Name is Must"
+                nameTextInputLayout?.error = "First & Last Name are Must"
             }
         }
         nameTextInputLayout?.isErrorEnabled = true
         nameTextInputLayout?.error = "Name is Must"
+        return false
+    }
+
+    private fun validateAge(): Boolean {
+        if (!TextUtils.isEmpty(ageTextInputEditText?.text)) {
+            if((ageTextInputEditText?.text.toString().toInt()) > 4 && (ageTextInputEditText?.text.toString().toInt()) < 80){
+                ageTextInputLayout?.isErrorEnabled = true
+                ageTextInputLayout?.error = "Invalid Age"
+            }else{
+                return true
+            }
+        }
+        ageTextInputLayout?.isErrorEnabled = true
+        ageTextInputLayout?.error = "Age is Must"
         return false
     }
 
